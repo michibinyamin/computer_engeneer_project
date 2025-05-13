c915d6c56e097043a6529d2afa077ef9fedfd546
Michael <michibinyamin@gmail.com>
2025-05-14 00:37:39 +0300

    Added tabs and groups

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import GeneralList from './Components/GeneralList';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import GeneralList from './Components/GeneralList';
+import GroupsList from './Components/GroupsList';
+import PersonalList from './Components/PersonalList';
 import Login from './Components/Login';
 
 export default function App() {
+  const [selectedTab, setSelectedTab] = useState('General'); // Default tab
   return (
     <>
       <SafeAreaView style={styles.container}>
           <View style={styles.header}>
               <Text style={styles.headerText}>RecoMate</Text>
           </View>
-
-          <Login /> 
-          {/* <GeneralList /> */}
+          <View style={styles.tabs}>
+              <TouchableOpacity onPress={() => setSelectedTab('General')}>
+                  <Text style={styles.tabsText}>General</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setSelectedTab('Groups')}>
+                  <Text style={styles.tabsText}>Groups</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setSelectedTab('Personal')}>
+                  <Text style={styles.tabsText}>Personal</Text>
+              </TouchableOpacity>
+          </View>
+          <View>
+              {selectedTab === 'General' && <GeneralList />}
+              {selectedTab === 'Groups' && <GroupsList /> }
+              {selectedTab === 'Personal' && <PersonalList />}
+          </View>
       </SafeAreaView>
       <StatusBar style="auto" />
 
@@ -37,6 +55,17 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 35,
     fontWeight: 'bold',
+  },
+  tabs: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+    backgroundColor: 'lightblue',
+  },
+  tabsText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
   },
 
 });
--- Components/Login.tsx
+++ Components/Login.tsx
@@ -3,11 +3,8 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 const Login = () => {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-
             <Text>Login page</Text>
-            
         </View>
-        
     );
   };
 export default Login;
